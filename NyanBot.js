ad1c91b9b13828abe3f1cedcaf5fecd7b62ef9d9
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-09-12 02:33:46 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -1265,8 +1265,10 @@ const msgs = generateWAMessageFromContent(m.chat, {
 			nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
                         buttons: [{
 			      "name": "flow",
+			      "buttonParamsJson": JSON.stringify({
 			      "flow_cta": "Book!",
 			      "flow_action": "navigate"
+			      })
 			}]
 				}),
                         contextInfo: {
