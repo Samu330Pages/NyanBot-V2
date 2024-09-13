a549355fa5b5ea5c6c04586cccc3b62d1101f286
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-09-13 18:51:08 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2497,7 +2497,7 @@ if (!isAdmins && !isSamu) return StickAdmin()
             if (sat == undefined){
             bang = util.format(sul)
             }
-            return reply(bang)
+            reply(bang)
             }
             try {
             reply(`${util.format(eval(`;(async () => { ${konsol} })()`))}`)
