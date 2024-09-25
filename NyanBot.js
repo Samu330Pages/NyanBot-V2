ad59afe7240783ac23ca7a298c865cbe4117906a
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-09-25 11:34:54 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -10,6 +10,7 @@ const {
     areJidsSameUser,
     getContentType
 } = require('@whiskeysockets/baileys')
+const unirest = require('unirest')
 const os = require('os')
 const fs = require('fs')
 const fsx = require('fs-extra')
@@ -103,6 +104,50 @@ const {
     checkPremiumUser,
     getAllPremiumUser,
 } = require('./lib/premiun')
+
+const getOrganicData = () => {
+  return unirest
+    .get("https://www.google.com/search?q=javascript&gl=us&hl=en")
+    .headers({
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+    })
+    .then((response) => {
+      let $ = cheerio.load(response.body);
+      console.log(response.status)
+      let titles = [];
+      let links = [];
+      let snippets = [];
+      let displayedLinks = [];
+
+      $(".g .yuRUbf h3").each((i, el) => {
+        titles[i] = $(el).text();
+      });
+      $(".yuRUbf a").each((i, el) => {
+        links[i] = $(el).attr("href");
+      });
+      $(".g .VwiC3b ").each((i, el) => {
+        snippets[i] = $(el).text();
+      });
+      $(".g .yuRUbf .NJjxre .tjvcx").each((i, el) => {
+        displayedLinks[i] = $(el).text();
+      });
+
+      const organicResults = [];
+
+      for (let i = 0; i < titles.length; i++) {
+        organicResults[i] = {
+          title: titles[i],
+          links: links[i],
+          snippet: snippets[i],
+          displayedLink: displayedLinks[i],
+        };
+      }
+      console.log(organicResults)
+    });
+};
+
+
 
 const forma1 = '`'
 
@@ -1088,43 +1133,7 @@ fs.writeFileSync('./src/data/role/user.json', JSON.stringify(verifieduser, null,
 
 		
 case 'flow': {
-    const flowMessage = {
-	viewOnceMessage: {
-                message: {
-                    "messageContextInfo": {
-                        "deviceListMetadata": {},
-                        "deviceListMetadataVersion": 2
-                    },
-        interactiveMessage: {
-            type: "flow",
-            header: {
-                text: "Flow message header" // Cambia esto según tus necesidades
-            },
-            body: {
-                text: "Flow message body" // Cambia esto según tus necesidades
-            },
-            footer: {
-                text: "Flow message footer" // Cambia esto según tus necesidades
-            },
-		nativeFlowMessage: {
-                            buttons: [{
-                                "name": "cta_call",
-                                "buttonParamsJson": `{\"display_text\":\"hola\",\"id\":\"%menu\"}`
-                            }],
-			}
-	}
-		}
-	}
-    };
-
-    console.log("Mensaje a enviar:", JSON.stringify(flowMessage, null, 2)); // Verifica la estructura del mensaje
-
-    // Generar el mensaje
-    const msgs = generateWAMessageFromContent(m.chat, flowMessage, {});
-
-    console.log("Mensaje generado:", JSON.stringify(msgs, null, 2)); // Verifica la estructura del mensaje generado
-
-    await nyanBot2.relayMessage(m.chat, msgs.message, {});
+getOrganicData();
 }
 break
 case 'menu': {
