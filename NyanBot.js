1c9252cf1750eb32e00deb87e87da6f06375c87f
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-09-26 05:51:17 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -1754,7 +1754,7 @@ case 'yts': {
         const results = await yts(text);
 
         // Limitar a los primeros 10 resultados
-        const limitedResults = results.slice(0, 10);
+        const limitedResults = results.all.slice(0, 10);
 
         // Enviar cada resultado en un carrusel
         for (const video of limitedResults) {
