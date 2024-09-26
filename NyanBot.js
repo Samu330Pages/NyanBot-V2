de0da9f1389a6405d48515e8bf546ec8906d0feb
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-09-25 21:23:12 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -297,7 +297,7 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
     : (m.mtype === 'templateButtonReplyMessage') 
     ? m.message.templateButtonReplyMessage.selectedId 
     : (m.type === 'interactiveResponseMessage')
-    ? (JSON.parse(m.message.nativeFlowResponseMessage.paramsJson)).id : ''
+    ? m.message.nativeFlowResponseMessage.paramsJson.id : ''
 	var budy = (typeof m.text == 'string' ? m.text : '')
         //prefix 1
         var prefix = ['.', '/'] ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : xprefix
