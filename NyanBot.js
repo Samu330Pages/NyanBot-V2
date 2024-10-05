0870bd336d0e5239ef197a84dcc698b4d2e41078
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-05 09:45:48 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2142,9 +2142,9 @@ case 'insta': case 'ig': case 'instagram': {
 
         if (result.length > 1) {
             const numImages = Math.sqrt(result.length);
-            await reply(`_*Esperé un momento... sus imágenes se están enviando..._*\n> ${botname} by ${ownername}`)
+            await reply(`_*Sus imágenes se están enviando...*_\n> ${botname} by ${ownername}`)
             for (let i = 0; i < numImages; i++) {
-                if (result[i].thumbnail.includes('.jpg') || result[i].thumbnail.includes('.png')) {
+                if (result[i].url.includes('.jpg') || result[i].url.includes('.png')) {
                     const imageBuffer = await fetchBuffer(result[i].url);
                     await nyanBot2.sendMessage(m.chat, {
                         image: imageBuffer,
