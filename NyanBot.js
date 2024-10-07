3276ca6f814507689a047840fddb73f5b9bfb55b
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-07 02:56:50 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2621,10 +2621,10 @@ break
 			
 case 'pet+': {
 if (!text.includes(sender)) return reply('*Esta acción no te corresponde porque no es tu mascota! 🙂*');
-reply(text)
 const petName = args[0];
 const petType = args[1];
-const pet = createOrGetPet(sender, petName, petType);
+const petOwn = args[2];
+const pet = createOrGetPet(petOwn, petName, petType);
 reply(`*¡🥳 Felicidades, has creado a ${pet.name}, tu nueva mascota!*\n\n_*Para atender a tu amiguito y ver su estado, puedes usar el comando: ${prefix}pet*_`);
 }
 break
