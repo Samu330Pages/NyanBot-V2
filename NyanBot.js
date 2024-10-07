9324ee9370e4f8a7da7ac8a367444721c3a88d81
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-06 19:39:53 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -333,17 +333,35 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
         var budy = (typeof m.text == 'string' ? m.text : '')
         //prefix 1
         var prefix = ['.', '/'] ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : xprefix
-        const isCmd = body.startsWith(prefix, '')
+        //const isCmd = body.startsWith(prefix, '')
+
+	// Verificar si hay un prefijo y obtener el primer coincidido
+if (matchedPrefix) {
+    prefix = matchedPrefix[0];
+} else {
+    prefix = ""; // Si no se encuentra, dejarlo vacío
+}
+
+// Verificar si es un comando
+const isCmd = body.startsWith(prefix);
+const trimmedBody = body.slice(prefix.length).trim(); // Eliminar el prefijo y recortar espacios
+
+if (isCmd) {
+    const command = trimmedBody.split(/ +/).shift().toLowerCase(); // Comando
+    const args = trimmedBody.split(/ +/).slice(1); // Argumentos
+    const text = args.join(" "); // Texto completo
+
+	
         const isCmd2 = body.startsWith(prefix)
-        const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+        //const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
         const command2 = body.slice(1).trim().split(/ +/).shift().toLowerCase()
-        const args = body.trim().split(/ +/).slice(1)
+        //const args = body.trim().split(/ +/).slice(1)
         const full_args = body.replace(command, '').slice(1).trim()
         const pushname = m.pushName || "No Name"
         const botNumber = await nyanBot2.decodeJid(nyanBot2.user.id)
         const itsMe = m.sender == botNumber ? true : false
         const sender = m.sender
-        const text = q = args.join(" ")
+        //const text = q = args.join(" ")
         const from = m.key.remoteJid
         const fatkuns = (m.quoted || m)
         const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
