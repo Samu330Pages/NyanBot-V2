fca1142dc4a96292d2e88cd9f20d19172b8d16c6
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-14 22:24:39 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -213,6 +213,7 @@ const categories = {
         { command: 'ytmp4', description: '_*URL*_' },
         { command: 'tiktok', description: '_*URL*_' },
         { command: 'tt', description: '_*URL*_' },
+	{ command: 'tweter', description: '' },
         { command: 'facebook', description: '_*URL*_' },
         { command: 'fb', description: '_*URL*_' },
         { command: 'instagram', description: '_*URL*_' },
@@ -2228,6 +2229,39 @@ case 'insta': case 'ig': case 'instagram': {
 }
 break
 
+case 'x': case 'twiter': case 'tw':
+if (db.data.users[sender].limit < 1) return reply(mess.limit);
+if (db.data.users[sender].limit < 10) return reply(`*Lo siento, pero este comando requiere 10 puntos, y tu cuenta tiene ${db.data.users[sender].limit}!*\n_Si deseas ganar más puntos, usa el comando ${forma1}${prefix}puntos${forma1} para ver de que manera ganar puntos_`);
+if (!text) return reply(`*Asegúrate de incluir junto al comando un link válido de Twitter 🐦*`)
+try {
+let xInfo = await axios.get(`https://api.dorratz.com/xdown?url=${text}`)
+if (!xInfo.data) return stcReac('error', `_*No sé encontró enlace de descarga*_ 🙃`)
+nyanBot2.sendMessage(m.chat, { react: { text: '🕒', key: m.key } });
+let xCap = `${forma1}X / TWEETER DL 💙${forma1}\n
+👍🏻 *Likes:* ${formatNumber(xInfo.data.likes)}
+💬 *Respuestas:* ${formatNumber(xInfo.data.replies)}
+🔃 *Retweets:* ${formatNumber(xInfo.data.retweets)}
+👤 *Autor:* ${xInfo.data.authorName}
+🔎 *Nombré de usuario:* ${xInfo.data.authorUsername}\n`
+if (xInfo.data.media.type === 'video') {
+await nyanBot2.sendMessage(m.chat, {
+video: {url: xInfo.data.media.url},
+mimetype: 'video/mp4',
+caption: xCap
+}, {quoted: m})
+} else {
+await nyanBot2.sendMessage(m.chat, {
+image: {url: xInfo.data.media.url},
+caption: xCap
+}, {quoted: m})
+}
+db.data.users[sender].limit -= 10;
+} catch (e) {
+nyanBot2.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+stcReac('error', '*Lo siento pero al parecer ha corrido un error! puedes volver a intentarlo 😁*')
+}
+break
+
 // Case para TikTok
 case 'tt': case 'tiktok': {
     if (db.data.users[sender].limit < 1) return reply(mess.limit);
@@ -2309,6 +2343,8 @@ sourceUrl: 'https://www.tiktok.com/@samu330ofc3?_t=8qPoVlCApvc&_r=1',
 break
 
 case 'apk':
+if (db.data.users[sender].limit < 1) return reply(mess.limit);
+if (db.data.users[sender].limit < 30) return reply(`*Lo siento, pero este comando requiere 30 puntos, y tu cuenta tiene ${db.data.users[sender].limit}!*\n_Si deseas ganar más puntos, usa el comando ${forma1}${prefix}puntos${forma1} para ver de que manera ganar puntos_`); 
 if (!text) return reply(`*❌ Por favor ingresa una solicitud a buscar junto con el comando*\n_*Ejemplo:*_\n\n${prefix+command} pubg`)
 try {
 let apkInfo = await axios.get(`https://api.dorratz.com/v2/apk-dl?text=${text}`)
@@ -2340,6 +2376,7 @@ sourceUrl: `https://www.tiktok.com/@samu330ofc3?_t=8qPoVlCApvc&_r=1`
 }
 }
 }, { quoted: m })
+db.data.users[sender].limit -= 30;
 nyanBot2.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
 } catch (e) {
 nyanBot2.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
