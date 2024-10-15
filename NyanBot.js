6e5cbec8b779fbb4dc944581c9f4271112b9bf1b
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-15 12:19:18 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -355,7 +355,7 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
         const botNumber = await nyanBot2.decodeJid(nyanBot2.user.id)
         const itsMe = m.sender == botNumber ? true : false
         const sender = m.sender
-        const text = body.startsWith(prefix) ? body.replace(/^\.\s*([a-zA-Z]+)/, '').trim() : body.trim()
+        const text = body.replace(/^\.\s*([a-zA-Z]+)/, '').trim()
         const from = m.key.remoteJid
         const fatkuns = (m.quoted || m)
         const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
