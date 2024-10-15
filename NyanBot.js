4b2b2d609af3116b4f65136a957484256c652356
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-15 11:40:29 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -347,7 +347,7 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
         var prefix = ['.', '/'] ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : xprefix
 	const isCmd = body.startsWith(prefix, '')
         const isCmd2 = body.startsWith(prefix)
-        const command = isCmd ? body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase() : ""
+        const command = isCmd ? body.replace(prefix, '').trim().split(/ +/)[0].toLowerCase() : ""
         const command2 = body.slice(1).trim().split(/ +/).shift().toLowerCase()
         const args = body.trim().split(/ +/).slice(1)
         const full_args = body.replace(command, '').slice(1).trim()
@@ -355,7 +355,7 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
         const botNumber = await nyanBot2.decodeJid(nyanBot2.user.id)
         const itsMe = m.sender == botNumber ? true : false
         const sender = m.sender
-        const text = isCmd ? body.replace(`${command} `, '').trim() : ""
+        const text = isCmd ? body.replace(`${prefix}${command} `, '').trim() : ""
         const from = m.key.remoteJid
         const fatkuns = (m.quoted || m)
         const quoted = (fatkuns.mtype == 'buttonsMessage') ? fatkuns[Object.keys(fatkuns)[1]] : (fatkuns.mtype == 'templateMessage') ? fatkuns.hydratedTemplate[Object.keys(fatkuns.hydratedTemplate)[1]] : (fatkuns.mtype == 'product') ? fatkuns[Object.keys(fatkuns)[0]] : m.quoted ? m.quoted : m
