9060e564748cb48df097c0526b26bc970e11d614
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-16 12:44:51 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2361,7 +2361,7 @@ caption: `${forma1}DESCARGA COMPLETA 📱${forma1}\n
 *Paquete:* ${apkInfo.data.package}
 *Última actualización:* ${apkInfo.data.lastUpdate}\n
 > ${botname}`,
-jpegThumbnail: await fetchBuffer(apkInfo.data.icon),
+//jpegThumbnail: await fetchBuffer(apkInfo.data.icon),
 contextInfo: {
 externalAdReply: {
 showAdAttribution: true,
