b6878cc665ed5cd2c828b3762f7dd05dce5f180b
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-18 11:40:54 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2106,7 +2106,7 @@ case 'music': case 'song': case 'whatmusic': {
     if (db.data.users[sender].limit < 1) return reply(mess.limit);
     if (db.data.users[sender].limit < 50) return reply(`*Lo siento, pero este comando requiere 50 puntos, y tu cuenta tiene ${db.data.users[sender].limit}!*\n_Si deseas ganar más puntos, usa el comando ${forma1}${prefix}puntos${forma1} para ver de que manera ganar puntos_`);
     if (!m.quoted) return reply('Responde a un audio o video con el comando para reconocer la canción.');
-    if (!/audio/.test(mime) || !/video/.test(mime)) return reply('*No as etiquetado un audio/video, por favor asegurate de etiquetar el audio a reconocer junto al comando!*');
+    //if (!/audio/.test(mime)) return reply('*No as etiquetado un audio/video, por favor asegurate de etiquetar el audio a reconocer junto al comando!*');
     let buffS = ""
     if (/audio/.test(mime)) {
     buffS = await nyanBot2.downloadAndSaveMediaMessage(quoted, 'music');
