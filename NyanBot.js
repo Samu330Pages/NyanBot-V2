771b34095163490610a02607e3fa8075aa86749c
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-19 11:03:09 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2095,9 +2095,9 @@ case 'ytvlink': {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const data = await response.json();
-	    await reply(`${data}`)
-            const videoBuffer = await fetchBuffer(data.data.mp4);
+        //const data = await response.json();
+	    await reply(JSON.stringify(response, null, 2))
+            const videoBuffer = await fetchBuffer(response.data.mp4);
             await nyanBot2.sendMessage(m.chat, {
                 document: videoBuffer,
                 fileName: `video.mp4`,
