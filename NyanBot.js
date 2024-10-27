a4e4a16edc83240597320bb54e16a852cde37794
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-26 21:46:45 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2385,20 +2385,19 @@ case 'ytv': {
 }
 break
 
-case 'cuevana': {
+case 'cuevana': case 'pelis': {
     if (!text) return reply('_*¿Qué película estás buscando?*_');
     
     let searchResults = await require("./lib/cuevana.js").cuevana(text);
     
-    let responseText = `*👤 Autor: ${searchResults.author.author}*\n`;
-    responseText += `*🌐 Página: ${searchResults.author.page}*\n\n`;
+    let responseText = `${forma1} CUEVANA SEARCH 📼${forma1}\n\n`;
     responseText += `*Resultados de búsqueda para "${text}":*\n\n`;
 
     if (searchResults.results.length === 0) {
         responseText += `_*No se encontraron resultados para "${text}".*_`;
     } else {
         searchResults.results.forEach((movie, index) => {
-            responseText += `*${index + 1}. ${movie.title}*\n`;
+            responseText += `*${forma1}${index + 1}. ${movie.title}${forma1}*\n\n`;
             responseText += `*Enlace:* ${movie.link}\n`;
             responseText += `📍--------------\n\n`;
         });
