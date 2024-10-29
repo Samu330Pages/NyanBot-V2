6267dff694c4dfc2b86b952a810c4b736f99fd17
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-10-29 11:14:00 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -1921,7 +1921,7 @@ case 'ytmp32':
 case 'yta2': {
 const caseYtmp3 = require('./cases/ytmp3');
 await caseYtmp3(text, m, reply, nyanBot2, formatNumber, useLimit, sender, db, command, forma1, prefix);
-break;
+break
 			
 case 'ytmp3':
 case 'yta': {
