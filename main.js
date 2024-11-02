3f3de7eb1ea16babc1212227895bd187605854b0
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-01 20:19:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ require('./main.js')
 nocache('../main.js', module => console.log(color('[ CHANGE ]', 'green'), color(`'${module}'`, 'green'), 'Updated'))
 
 //------------------------------------------------------
-let phoneNumber = "916909137213"
+let phoneNumber = "5219984907794"
 let owner = JSON.parse(fs.readFileSync('./src/data/role/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
