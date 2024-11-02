204f8f0c16cd813305ee15fd6e588e1f03a6b26d
IIDK <neirmossoftware@gmail.com>
2024-11-02 13:02:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const {
 } = require('child_process')
 const path = require('path')
 
+const unhandledRejections = new Map()
+process.on('unhandledRejection', (reason, promise) => {
+   unhandledRejections.set(promise, reason)
+   console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+})
+process.on('rejectionHandled', (promise) => {
+   unhandledRejections.delete(promise)
+})
+process.on('Something went wrong', function(err) {
+   console.log('Caught exception: ', err)
+})
+
 function start() {
 	let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
 	let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
