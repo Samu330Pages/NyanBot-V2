6ba4d31ee5b0a81a432b3d21f0893342c889b62c
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-03 00:50:45 -0500

    Create acount-logout.js

---
+++ cases/acount-logout.js
@@ -0,0 +1,14 @@
+module.exports = async function(text, m, nyanBot2, sender, sendReplyButton, prefix) {
+if (global.DATABASE.data.users[sender].register === false) return reply('*No fue posible cerrar tu sesión, porque aún no la has iniciado!*')
+                nyanBot2.sendMessage(m.chat, { react: { text: '😫', key: m.key } })
+                const buttons = [{
+                    name: "quick_reply",
+                    buttonParamsJson: JSON.stringify({
+                        display_text: 'Si',
+                        id: `${prefix}lg ${sender}`
+                    }),
+                }];
+                sendReplyButton(m.chat, buttons, m, {
+                    content: `*Estás seguro que deseas cerrar tu sesión en el bot?* ⚠`
+                });
+};
