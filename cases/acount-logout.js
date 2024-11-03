eb9f010e6f8f56cedf89477a85a00142f8cd4a96
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-03 01:07:02 -0500

    Update acount-logout.js

--- cases/acount-logout.js
+++ cases/acount-logout.js
@@ -1,5 +1,5 @@
 module.exports = async function(text, m, reply, nyanBot2, sender, sendReplyButton, prefix, db) {
-if (db === false) return reply('*No fue posible cerrar tu sesión, porque aún no la has iniciado!*')
+if (db == false) return reply('*No fue posible cerrar tu sesión, porque aún no la has iniciado!*')
                 nyanBot2.sendMessage(m.chat, { react: { text: '😫', key: m.key } })
                 const buttons = [{
                     name: "quick_reply",
