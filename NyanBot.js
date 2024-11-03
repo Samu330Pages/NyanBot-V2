98397874e43d0d6941707aca56bed9b4fc7e43a2
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-03 01:00:38 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -1040,7 +1040,7 @@ TE DESCRIBO PARA QUE SIRVE CADA COMANDO 😁:\n`
                 break
             case 'logout': {
                 const caseLogout = require('./cases/acount-logout');
-                await caseLogout(text, m, reply, nyanBot2, sender, sendReplyButton, prefix);
+                await caseLogout(text, m, reply, nyanBot2, sender, sendReplyButton, prefix, `${db.data.users[sender].register}`);
             }
                 break
             case 'login': {
