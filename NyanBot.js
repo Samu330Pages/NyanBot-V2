a8303d54a06a328c2c9fd3d54a535dc4cbeb5970
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-04 12:32:02 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -875,12 +875,12 @@ if (juegoActivoIndex !== -1) {
 
     db.data.game.soup = userGames;
 }
-*/
+
 const instrucciones = `*Instrucciones del juego:* \n\n- Encuentra las palabras ocultas en la sopa de letras.\n- Cada vez que aciertes una palabra, ganarás 100 puntos.\n- Si encuentras las tres palabras, ganarás un total de 400 puntos.\n- ¡Diviértete!`;
 await nyanBot2.sendMessage(m.chat, {
     image: juegoActivo.imagenResaltada,
     caption: `${instrucciones}\n\n*¡Buena suerte!*`
-});
+});*/
 
         switch (isCommand) {
 
