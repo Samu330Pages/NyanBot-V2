520bfb0f0d9f777487ad02e1bf6ef44b8954ec01
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-06 11:38:52 -0500

    Update play.js

--- cases/play.js
+++ cases/play.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const yts = require('yt-search');
 
-module.exports = async function(text, m, reply, isUrl, reactionLoad, reactionOk, reactionError, sendMessage, formatNumber, prefix) {
+module.exports = async function(text, m, reply, isUrl, reactionLoad, reactionOk, reactionError, nyanBot2, formatNumber, prefix, readmore) {
     if (!text) return reply(`Ejemplo: ${prefix}play piel canela`);
     if (isUrl(text)) return reply(`Para descargar audio desde el link de YouTube, utiliza el comando:\n\n${prefix}ytmp3`);
 
@@ -14,21 +14,19 @@ module.exports = async function(text, m, reply, isUrl, reactionLoad, reactionOk,
 
     const video = r.videos[0];
 
-    // Preparar el mensaje con la información del video
     const caption = `> *YT Play 🍟.*\n\n` +
                     `- *Título:* ${video.title}\n` +
                     `- *Duración:* ${video.timestamp}\n` +
                     `- *Autor:* ${video.author.name}\n` +
                     `- *Vistas:* ${formatNumber(video.views)}\n\n` +
                     `Instrucciones:\n` +
-                    `Envía "v" para descargar el video.\n` +
-                    `Envía "a" para descargar el audio.\n` +
-                    `Puedes ver el video en la app aquí: ${video.url}`;
+                    `Etiqueta con "v" para descargar el video.\n` +
+                    `Etiqueta con "a" para descargar el audio.\n` +
+                    `${readmore}Link: ${video.url}`;
 
-    // Enviar el mensaje con la imagen y la información
-    await sendMessage(m.chat, {
-        caption: caption,
-        media: await (await fetch(video.thumbnail)).buffer()
+    await nyanBot2.sendMessage(m.chat, {
+        image: await (await fetch(video.thumbnail)).buffer(),
+        caption: caption
     }, { quoted: m });
 
     reactionOk(m.chat, m.key, playId);
