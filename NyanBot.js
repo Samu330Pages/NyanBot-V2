8c9f97413bf588a065afe6f0160e7286ca2b4fd6
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-07 08:56:58 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -833,7 +833,7 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
                 if (isSamu) return
 		if (db.data.users[sender].link == 5) {
 			db.data.users[sender].link = 0
-			return await nyanBot2.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+			await nyanBot2.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 		}
                 await nyanBot2.sendMessage(m.chat,
                     {
@@ -844,8 +844,8 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
                             participant: m.key.participant
                         }
                     })
-                nyanBot2.sendMessage(from, { text: `\`\`\`「 Link Detected 」\`\`\`\n\n@${m.sender.split("@")[0]} *En este grupo no está permitido el envió de links de otros grupos!!*\n_Advertencia N° *${db.data.users[sender].link},* a la 5ta seras eliminado!_`, contextInfo: { mentionedJid: [m.sender] } }, { quoted: m })
 		db.data.users[sender].link += 1;
+                nyanBot2.sendMessage(from, { text: `\`\`\`「 Link Detected 」\`\`\`\n\n@${m.sender.split("@")[0]} *En este grupo no está permitido el envió de links de otros grupos!!*\n\n_Advertencia N° *${db.data.users[sender].link},* a la 5ta seras eliminado!_`, contextInfo: { mentionedJid: [m.sender] } }, { quoted: m })
             }
         }
 
