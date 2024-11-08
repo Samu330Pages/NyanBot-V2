ae830274335b36c3802fbe4048b2758215211648
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-08 09:39:03 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -852,9 +852,9 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
             fs.writeFileSync('./src/data/role/user.json', JSON.stringify(verifieduser, null, 2))
         }
 
-if (m.quoted && m.quoted.text && /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/.test(m.quoted.text)) {
+if (m.quoted && m.quoted.text && /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/.test(m.quoted.text)) {
     const quotedText = m.quoted.text;
-    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const matches = quotedText.match(regex);
 
     const videoLink = matches[0];
