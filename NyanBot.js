0fbf38efb53cc42fe68010ccd55a0c597525dfdd
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-08 13:53:14 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -939,22 +939,8 @@ if (juegoActivoIndex !== -1) {
 		break
 
 case 'w':
-let r = canvaImg.createWelcomeImage("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60")
-await nyanBot2.sendMessage(m.chat, {
-text: date,
-contextInfo: {
-externalAdReply: {
-renderLargerThumbnail: true,
-mediaType: 1,
-title: date,
-body: '',
-thumbnail: r,
-jpegThumbnail: r,
-previewType: "NONE",
-sourceUrl: 'https://chat.whatsapp.com/GtG0Q6rBVTTGAz8GmfS3e1',
-}
-}
-}, { quoted: m })
+let r = await canvaImg.createWelcomeImage("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60")
+await nyanBot2.sendMessage(m.chat, {image: r}, { quoted: m })
 break
 
             case 'menu': {
