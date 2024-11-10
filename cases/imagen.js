e0126d75d70f12480aa9c54d8e212fd983edaf12
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 02:45:59 -0500

    Update imagen.js

--- cases/imagen.js
+++ cases/imagen.js
@@ -46,7 +46,7 @@ module.exports = async function(m, reply, nyanBot2, text, prefix, command, stcRe
             }
         });
         console.error('Error en la búsqueda de imágenes:', error);
-        reply(error)
+        reply(`${error}`)
         stcReac('error', `_*❌ Ha ocurrido un error!*_\n*Intenta de nuevo porfavor! 🙂*`)
     }
 };
