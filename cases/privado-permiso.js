27cdcae23b9b07acb162836b2a4df6104c4d1229
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 01:56:58 -0500

    Update privado-permiso.js

--- cases/privado-permiso.js
+++ cases/privado-permiso.js
@@ -2,6 +2,6 @@ module.exports = async function(m, reply, sender, text, isSamu) {
     if (!isSamu) return reply(mess.bot)
     let permNum = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     if (!permNum) return reply('*Por favor etiqueta el mensaje de la persona que deseas permitirle el uso privado!*')
-    global.DATABASE.data.users[sender].priv = true
+    global.DATABASE.data.users[permNum].priv = true
     reply('*Permiso concedido, el usuario ya puede hacer uso privado del bot!*')
 };
