8f6fe60811d9afb874158084cd73dc133965c2bf
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 18:10:44 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2049,7 +2049,7 @@ case 'mediafire': case 'mf': {
         }
 
         await nyanBot2.sendMessage(m.chat, {
-            document: {url: `${data.link}`},
+            document: await (await fetch(`${data.link}`)).buffer();,
             fileName: `${data.filename}`,
             mimetype: `${data.mime}`,
             caption: `
