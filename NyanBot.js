f41a0e5473cf3a06bc4a89036ab06ace38690e85
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 02:37:55 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -964,38 +964,9 @@ if (juegoActivoIndex !== -1) {
 
             case 'img':
             case 'imagen':
-            case 'imagenes': {
-                const query = text || m.quoted?.text;
-                if (!query) {
-                    return reply(`Por favor, proporciona un término de búsqueda de imágenes.\n*Ejemplo:* ${prefix + command} gatos`);
-                }
-
-                nyanBot2.sendMessage(m.chat, { react: { text: '🕒', key: m.key } });
-
-                try {
-                    let r = await fg.googleImage(query);
-                    if (r.length === 0) {
-                        return reply("No se encontraron imágenes para la búsqueda proporcionada.");
-                    }
-
-                    const sendRandomImage = async () => {
-                        const randomIndex = Math.floor(Math.random() * r.length);
-                        const imageUrl = r[randomIndex];
-
-                        await nyanBot2.sendMessage(m.chat, {
-                            image: await (await fetch(imageUrl)).buffer(),
-                            caption: `*🍟 Resultado de tu búsqueda:*\n${query}\n`
-                        }, { quoted: m })
-                    };
-                    await sendRandomImage();
-
-                    nyanBot2.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
-                } catch (error) {
-                    nyanBot2.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
-                    console.error('Error en la búsqueda de imágenes:', error);
-                    stcReac('error', `_*❌ Ha ocurrido un error!*_\n*Intenta de nuevo porfavor! 🙂*`)
-                }
-            }
+            case 'imagenes':
+                const caseImagen = require('./cases/imagen');
+                await caseImagen(m, reply, nyanBot2, text, prefix, command, stcReac);
                 break
 
             case 'color': case 'recolor': {
