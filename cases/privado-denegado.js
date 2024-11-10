15cdd9e05d12219164b31333deb9370940b74c8d
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 01:56:48 -0500

    Update privado-denegado.js

--- cases/privado-denegado.js
+++ cases/privado-denegado.js
@@ -2,6 +2,6 @@ module.exports = async function(m, reply, sender, text, isSamu) {
     if (!isSamu) return reply(mess.bot)
     let permDNum = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     if (!permDNum) return reply('*Por favor etiqueta el mensaje de la persona que deseas permitirle el uso privado!*')
-    db.data.users[sender].priv = false
+    db.data.users[permDNum].priv = false
     reply('*Permiso denegado, el usuario ya no puede hacer uso privado del bot!*')
 };
