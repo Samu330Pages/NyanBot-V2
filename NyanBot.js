c2ef3d815d0450ba1081fc4242d4ccded43325fa
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 02:52:28 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -969,16 +969,9 @@ if (juegoActivoIndex !== -1) {
                 await caseImagen(m, reply, nyanBot2, text, prefix, command, stcReac);
                 break
 
-            case 'color': case 'recolor': {
-                if (!quoted) return replygcxeon(`*Porfavor etiqueta una imagen con el comando para poder realizar el aumento de calidad!* 🖼️`)
-                if (!/image/.test(mime)) return reply(`*por favor envía o etiqueta una imagen junto con el comando:* ${prefix + command}`)
-                nyanBot2.sendMessage(m.chat, { react: { text: '🕒', key: m.key } });
-                const { remini } = require('./lib/remini')
-                let media = await quoted.download()
-                let recolor = await remini(media, "recolor")
-                nyanBot2.sendMessage(m.chat, { image: recolor, caption: mess.success }, { quoted: m })
-                nyanBot2.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
-            }
+            case 'color': case 'recolor':
+                const caseColor = require('./cases/imagen');
+                await caseColor(m, reply, nyanBot2, prefix, command, quoted, mime);
                 break
 
             case 'remini': case 'hd': {
