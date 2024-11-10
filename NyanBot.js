745671539d464e167021b11a749cecbd1124c417
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-10 00:48:57 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -924,26 +924,14 @@ let r = await canvaImg.createWelcomeImage("https://cdn.pixabay.com/photo/2015/10
 await nyanBot2.sendMessage(m.chat, {image: r}, { quoted: m })
 break
 
-            case 'menu':
+            case 'menu': case 'comandos'
                 const caseMenu = require('./cases/menu');
                 await caseMenu(m, reply, nyanBot2, sender, categories, checkPremiumUser, botNumber);
                 break
-
+		
 		case 'ayuda': case 'help':
-                let helpMsg = `😊 _*AQUÍ TE EXPLICO COMO USAR LAS FUNCIONES DEL BOT!*_
-_PRIMERO DEBES SABER QUE PARA USAR UN COMANDO DEBES PRIMERO ESCRIBIR EL PREFIJO, QUE EN ESTE CASO ES_ 👉🏻 *${prefix}* 👈🏻, _SEGUIDAMENTE VA EL COMANDO QUE DESEES USAR, EJEMPLO:_
-
-*${prefix}menu*
-
-TE DESCRIBO PARA QUE SIRVE CADA COMANDO 😁:\n`
-                for (const [category, help] of Object.entries(categories)) {
-                    helpMsg += `*${category}:*\n`;
-                    help.forEach(cmdObj => {
-                        helpMsg += `- ${forma1}${cmdObj.command}:${forma1} _*${cmdObj.help}*_\n`;
-                    });
-                    helpMsg += '\n';
-                }
-                reply(helpMsg)
+                const caseAyuda = require('./cases/menu');
+                await caseAyuda(m, reply, sender, prefix, categories);
                 break
 
             case 'claim':
