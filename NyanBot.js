24b6e6fe8799cbbd399f748acdb8e9068bbfd0b3
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-12 17:01:44 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -907,22 +907,10 @@ if (juegoActivoIndex !== -1) {
 		reply(resultado)
 		break
 
-		case 'get':
-			if (m.quoted && m.quoted.chat == 'status@broadcast') {
-			let statusMedia = await quoted.download()
-			if (/image/.test(m.quoted.mtype)) {
-			await nyanBot2.sendMessage(m.chat, {image: statusMedia, caption: m.quoted.caption}, {quoted: m})
-			} else if (/video/.test(m.quoted.mtype)) {
-			await nyanBot2.sendMessage(m.chat, {video: statusMedia, caption: m.quoted.caption}, {quoted: m})
-			} else if (/audio/.test(m.quoted.mtype)) {
-			await nyanBot2.sendMessage(m.chat, {audio: statusMedia, caption: m.quoted.caption, ptt: true}, {quoted: m})
-			} else {
-			await reply(m.quoted.text)
-			}
-			} else {
-			return reply('*Responde a un estado con este comando para descargar el estado del contacto! 🕵🏻*')
-			}
-			break
+	    case 'get':
+		const caseGetStatus = require('./cases/getStatus');
+		await caseGetStatus()
+		break
 
             case 'menu': case 'comandos':
                 const caseMenu = require('./cases/menu');
