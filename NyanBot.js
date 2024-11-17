1a33a4e53474020786c8d9fcbb951f55424e1252
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-17 02:46:04 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -1046,22 +1046,9 @@ if (juegoActivoIndex !== -1) {
                 await casePelis(text, m, reply, nyanBot2, sendCarousel, stcReac, command, prefix);
                 break
 
-            case 'pelidl': {
-                if (!text) {
-                    return reply(`*Por favor, proporciona un link de descarga. Ejemplo:*\n\n${prefix + command} [link]`);
-                }
-                const [link, movieName] = text.split('|');
-                if (!link) {
-                    return reply(`*No se encontró un link válido.*`);
-                }
-                const caption = `Para descargar la película, necesitas una aplicación de torrent.`;
-                nyanBot2.sendMessage(m.chat, {
-                    document: { url: link },
-                    mimetype: 'application/x-bittorrent',
-                    caption: caption,
-                    fileName: `${movieName}.torrent`
-                });
-            }
+            case 'pelidl':
+		const casePelisDl = require('./cases/pelis-dl');
+                await casePelisDl(text, m, reply, nyanBot2, command, prefix);
                 break
 
             case 'cuevana': {
