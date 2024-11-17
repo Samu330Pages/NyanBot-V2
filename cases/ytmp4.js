b72888b35ce5f35cc5343cd514bbdb799a115aa3
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-17 01:32:46 -0500

    Update ytmp4.js

--- cases/ytmp4.js
+++ cases/ytmp4.js
@@ -6,7 +6,7 @@ const {
 } = require('../lib/samufuncs')
 const forma1 = '`'
 
-module.exports = async function(text, m, reply, nyanBot2, useLimit, stcReac, sender, db, command, prefix) {
+module.exports = async function(text, m, reply, nyanBot2, useLimit, stcReac, sender, command, prefix) {
     if (global.DATABASE.data.users[sender].limit < 1) return reply(global.mess.limit);
     if (global.DATABASE.data.users[sender].limit < 30) return reply(`*Lo siento, pero este comando requiere 30 puntos, y tu cuenta tiene ${global.DATABASE.data.users[sender].limit}!*\n_Si deseas ganar más puntos, usa el comando ${forma1}${prefix}puntos${forma1} para ver de que manera ganar puntos_`);
 
