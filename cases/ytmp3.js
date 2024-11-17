ef7a3e9bd9b56c84e4fe900888d1b579e79cf8c0
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-17 01:37:43 -0500

    Update ytmp3.js

--- cases/ytmp3.js
+++ cases/ytmp3.js
@@ -5,12 +5,12 @@ const {
     formatNumber
 } = require('../lib/samufuncs')
 
-module.exports = async function(link, m, text, reply, nyanBot2, useLimit, stcReac, sender, prefix) {
+module.exports = async function(link, m, text, reply, nyanBot2, useLimit, stcReac, sender, command, prefix) {
     if (global.DATABASE.data.users[sender].limit < 1) return reply(global.mess.limit);
     if (global.DATABASE.data.users[sender].limit < 30) return reply(`*Lo siento, pero este comando requiere 30 puntos, y tu cuenta tiene ${global.DATABASE.data.users[sender].limit}!*\n_Si deseas ganar más puntos, usa el comando ${forma1}${prefix}puntos${forma1} para ver de que manera ganar puntos_`);
 
     if (!/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/.test(link)) {
-        return reply(`*Es necesario un link válido de YouTube.*\n_*Ejemplo de uso*_\n\n${prefix} link...`);
+        return reply(`*Es necesario un link válido de YouTube.*\n_*Ejemplo de uso*_\n\n${prefix+command} link...`);
     }
 
     nyanBot2.sendMessage(m.chat, { react: { text: '🕑', key: m.key } });
