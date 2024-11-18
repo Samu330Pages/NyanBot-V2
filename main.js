1fa1024613c208012137526fe811caf2691eb2b7
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-18 04:09:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -336,9 +336,8 @@ const startNyanBot = async () => {
             for (let callStatus of callDetec) {
             if (callStatus.isGroup == false) {
             if (callStatus.status == "offer") {
-            //let callText = await nyanBot2.sendTextWithMentions(callStatus.from, `*${callStatus.user.name}* can't receive ${callStatus.isVideo ? `video` : `voice` } call. Sorry @${callStatus.from.split('@')[0]} you will be blocked. If called accidentally please contact the owner to be unblocked !`)
-            nyanBot2.sendMessage(callStatus.from, {text: 'No Calls'})
-            await sleep(1000)
+            let callText = await nyanBot2.sendTextWithMentions(callStatus.from, `*Lo siento @${callStatus.from.split('@')[0]},* las llamadas de ${callStatus.isVideo ? `*video*` : `*audio*` } estan bloqueadas 🚫!\n\n> AutoCallBlock For ${callStatus.user.name}!`)
+            await nyanBot2.sendMessage(callStatus.from, callText)
             await nyanBot2.rejectCall(callStatus.id, callStatus.from)
             }
             }
