46a84e5592be80d7499ad4d334e1be41e5ae4bf1
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-18 04:55:21 -0500

    Update anticall.js

--- cases/anticall.js
+++ cases/anticall.js
@@ -6,8 +6,10 @@ module.exports = async function(m, reply, sender, text, isSamu, botNumber) {
         db.data.settings[botNumber].anticall = true
         reply('*Activado exitosamente! Ahora se bloquearán todo tipo de llamadas.*')
     } else if (text == 'off' || 'OFF') {
-        if (!db.data.settings[botNumber].anticall) return reply('*Esta opción ya está desactivada.*')
+        if (db.data.settings[botNumber].anticall == false) return reply('*Esta opción ya está desactivada.*')
         db.data.settings[botNumber].anticall = false
         reply('*Desactivado exitosamente! Ahora se permitirán todo tipo de llamadas.*')
+    } else {
+        reply('*No se encontró opción adecuada, asegurate de solo usar `ON` y `OFF`*')
     }
 }
