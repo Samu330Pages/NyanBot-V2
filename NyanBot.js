63a8b768a3dc1b490c1cc5890e0ed17a144c0999
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-25 10:34:09 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -844,13 +844,33 @@ if (juegoActivoIndex !== -1) {
             }
             break
 			
-		case 'apk2':
-		if (!text) return reply("*Incluye junto al comando la aplicación que deseas buscar! 🙃*")
-		let r = require("./lib/apk-dl");
-		let i = await r.apkcombo.search(text);
-		let s = await require("./lib/canvaImg.js").createAppListImage(i, text);
-		await nyanBot2.sendMessage(from, { image: s, caption: `Para descargar, etiqueta este mensaje eh incluye el número de aplicación que desees y que este en la lista de la imagen! 😦` }, {quoted: m});
-		break
+case 'apk2':
+    if (!text) return reply("*Incluye junto al comando la aplicación que deseas buscar! 🙃*");
+    
+    let r = require("./lib/apk-dl");
+    let i = await r.apkcombo.search(text);
+
+    const limitedApps = i.slice(0, 6);
+
+    let caption = `${forma1}APKCOMBO DL 🕹️${forma1}\n\n📱 *Resultados de búsqueda para "${text}"*\n\n`;
+    
+    if (limitedApps.length === 0) {
+        caption += `❌ *No se encontraron aplicaciones.* ❌`;
+    } else {
+        limitedApps.forEach(app => {
+            caption += `◦  *${app.name}* - ${app.rating} (${app.downloads})\n`;
+        });
+	caption += `⚠️ _INSTRUCCIONES_ ⚠️: *Para descargar una aplicación, solo etiqueta este mensaje con el número correspondiente a la aplicación que deseas.*\n`;
+	caption += `_*Ejemplo:*_ ${forma1}2${forma1}`;
+        caption += `\n${readmore}🔗 *Links*\n`;
+        limitedApps.forEach(app => {
+            caption += `${app.link}\n`;
+        });
+    }
+
+    let s = await require("./lib/canvaImg.js").createAppListImage(i, text);
+    await nyanBot2.sendMessage(from, { image: s, caption: caption }, { quoted: m });
+    break
 			
 		case 'ruleta':
 		nyanBot2.sendMessage(m.chat, { react: { text: '🎡', key: m.key } });
