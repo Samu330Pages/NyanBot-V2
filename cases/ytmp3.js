cbe1ac4bf8b6e57993e894f702764743685605da
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-11-25 19:36:25 -0500

    Update ytmp3.js

--- cases/ytmp3.js
+++ cases/ytmp3.js
@@ -21,7 +21,7 @@ module.exports = async function(link, m, reply, nyanBot2, useLimit, stcReac, sen
         let r = await ytdl.sYtdl(link);
         
         const durationMinutes = Math.floor(r[0].duration / 60);
-        if (r[0].duration >= 3600) return reply(`*No se puede descargar este audio ya que supera el límite de duración, este video dura ${durationMinutes}*`);
+        if (r[0].duration >= 3600) return reply(`*No se puede descargar este audio ya que supera el límite de duración, este video dura ${durationMinutes} minutos*`);
         const publishDate = new Date(r[0].publishDate).toLocaleDateString();
 
         const audioBuffer = await (await fetch(`${r[0].url}`)).buffer();
