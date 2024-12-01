7b9dcd448129f9525d15378b2c56488a5531a6b6
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-12-01 04:11:56 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -361,7 +361,8 @@ module.exports = nyanBot2 = async (nyanBot2, m, chatUpdate, store) => {
                 premium: `${isPremium ? 'true' : 'false'}`,
                 limit: limitUser,
                 totalLimit: 0,
-                lastClaim: null
+                lastClaim: null,
+		rewards: []
             }
 
             let chats = global.db.data.chats[from]
@@ -859,6 +860,34 @@ if (m.quoted && m.quoted.text.startsWith(`${forma1}APKCOMBO DL 🕹️${forma1}`
 }
 
         switch (isCommand) {
+
+case 'rw':
+    if (!m.isGroup) return reply(mess.group);
+    //if (from !== '120363348063997699@g.us') return reply(`*Lo siento, pero las recompensas solo se pueden reclamar en el grupo oficial! 🥶*\nhttps://chat.whatsapp.com/GtG0Q6rBVTTGAz8GmfS3e1`);
+    
+    const responseRewards = await axios.get('https://samu330.com/nyan/rewards');
+    const html = responseRewards.data;
+    const $ = cheerio.load(html);
+    const jsonDataRewards = $('#json-data script[type="application/json"]').html();
+    const rewards = JSON.parse(jsonDataRewards);
+    
+    const code = text;
+    const reward = rewards.find(r => r.code === code);
+    
+    if (!reward) {
+        return reply(`🥶 El código de recompensa *${code}* no se encontró.\n*Porfavor asegurate de que el codigo exista y este disponible en la pagina https://samu330.com/nyan/rewards* 😊`);
+    }
+    
+    const puntos = reward.points;
+
+    if (db.data.users[sender].rewards.includes(code)) {
+        return reply(`Ya has reclamado esta recompensa. 😒`);
+    }
+    
+    db.data.users[sender].limit += puntos;
+    db.data.users[sender].rewards.push(code);
+    reply(`_*¡Felicidades! 🤯*_\n\n*Has canjeado una recompensa!!*\n\n- *${reward.name}*\n\nSe sumaron *${puntos}* puntos a tu cuenta!. 😊\n\n_Estate pendiente a la página para más recompensas!!_ 🥶`);
+    break
 
 		case 'grupo':
 		if (!m.isGroup) return reply(mess.group)
