c7caf27d753d660e68b280c71bed4a834797949b
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-12-09 10:45:04 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -2786,17 +2786,17 @@ break
                 neww = performance.now()
                 oldd = performance.now()
                 respon = `
-*Runtime* : ${runtime(process.uptime())}
+*🍋‍🟩 RUNTIME* : ${runtime(process.uptime())}
 
-*Info Server*
-RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+*💻 SERVIDOR*\n
+_*RAM:*_ ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
 
-_NodeJS Memory Usaage_\n
+_*Memoria NodeJS*_\n
 ${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${formatp(used[key])}`).join('\n')}
 
-${cpus[0] ? `_Total CPU Usage_
+${cpus[0] ? `_*Uso total del CPU*_\n
 ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
-_CPU Core(s) Usage (${cpus.length} Core CPU)_
+_*CPU Core(s) Usage (${cpus.length} Core CPU)*_\n
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
                 `.trim()
                 reply(`${respon}`)
