52deaf59b2f3f57522619d2a969375275f7033bd
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-12-19 12:02:00 -0500

    Update NyanBot.js

--- NyanBot.js
+++ NyanBot.js
@@ -923,25 +923,6 @@ if (m.quoted && m.quoted.text.startsWith(`${forma1}APKCOMBO DL 🕹️${forma1}`
 
         switch (isCommand) {
 
-			
-case 'lot':
-db.data.game.box[0] = randomBytes(10).toString('base64')
-nyanBot2.sendMessage("120363348063997699@g.us", {
-text: `\`MYSTERY BOX\`\n\n*SE HA DESPLEGADO UNA NUEVA CAJA MISTERIOSA!! 🔮*\n_*REACCIONA CON EL EMOJI ❤️ PARA ABRIR ESTA CAJA!!*_\n\n_Disponible solo para una persona, se habilitan cajas en tiempos aleatorios, así que estate atento a la próxima!_ ⚠️`,
-contextInfo: {
-"externalAdReply": {
-"containsAutoReply": true,
-"title": "NEW MYSTERY BOX HAS BEEN RELEASED! ✈️",
-"body": "Click aquí 👉🏻 [ ⭐ ]",
-"previewType": "PHOTO",
-"thumbnailUrl": ``,
-"thumbnail": await getBuffer('https://images2.wikia.nocookie.net/__cb20120729235044/thesimssocial/images/a/ae/Tycoon_mystery_box_icon.png'),
-"sourceUrl": `https://whatsapp.com/channel/0029VaDVQFVL7UVd71R7bY23`
-}
-}
-}, {messageId: "MysteryBox-" + randomBytes(8).toString('hex')})
-break
-
 case 'rw':
     if (!m.isGroup) return reply(mess.group);
     if (from !== '120363348063997699@g.us') return reply(`*Lo siento, pero las recompensas solo se pueden reclamar en el grupo oficial! 🥶*\nhttps://chat.whatsapp.com/GtG0Q6rBVTTGAz8GmfS3e1`);
@@ -1056,6 +1037,11 @@ case 'apk2':
 		break
 
 
+	    case 'lot':
+		const caseMysteryBox = require('./cases/mysteryBox');
+		await caseMysteryBox(nyanBot2, isSamu);
+		break
+			
 	    case 'todos': case 'tagall':
 		const caseTagAll = require('./cases/tagAll');
 		await caseTagAll(text, m, from, isAdmins, nyanBot2, groupMetadata);
